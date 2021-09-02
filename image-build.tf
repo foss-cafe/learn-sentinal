5885b0af21b425cda3b2ef95485dcdd56e5555bd
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-09-02 08:23:38 +0530

    Updating README file

--- image-build.tf
+++ image-build.tf
@@ -22,9 +22,9 @@ resource "aws_imagebuilder_component" "example" {
 
 resource "aws_imagebuilder_distribution_configuration" "example" {
   name = "example"
-  kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
   distribution {
     ami_distribution_configuration {
+      kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
       ami_tags = {
         CostCenter = "IT"
       }
