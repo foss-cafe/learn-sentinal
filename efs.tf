8837504e4ceff9cae674a5862b6bcaa9dc51dbce
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-09-02 08:20:28 +0530

    Updating README file

--- efs.tf
+++ efs.tf
@@ -1,43 +1,43 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_efs_file_system" "foo" {
-  creation_token = "my-product"
-  encrypted = true
+# resource "aws_efs_file_system" "foo" {
+#   creation_token = "my-product"
+#   encrypted = true
 
-  tags = {
-    Name = "MyProduct"
-  }
-}
+#   tags = {
+#     Name = "MyProduct"
+#   }
+# }
 
 
-resource "aws_efs_file_system_policy" "policy" {
-  file_system_id = aws_efs_file_system.foo.id
+# resource "aws_efs_file_system_policy" "policy" {
+#   file_system_id = aws_efs_file_system.foo.id
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Id": "ExamplePolicy01",
-    "Statement": [
-        {
-            "Sid": "ExampleStatement01",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Resource": "${aws_efs_file_system.foo.arn}",
-            "Action": [
-                "elasticfilesystem:ClientMount",
-                "elasticfilesystem:ClientWrite"
-            ],
-            "Condition": {
-                "Bool": {
-                    "aws:SecureTransport": "true"
-                }
-            }
-        }
-    ]
-}
-POLICY
-}
+#   policy = <<POLICY
+# {
+#     "Version": "2012-10-17",
+#     "Id": "ExamplePolicy01",
+#     "Statement": [
+#         {
+#             "Sid": "ExampleStatement01",
+#             "Effect": "Allow",
+#             "Principal": {
+#                 "AWS": "*"
+#             },
+#             "Resource": "${aws_efs_file_system.foo.arn}",
+#             "Action": [
+#                 "elasticfilesystem:ClientMount",
+#                 "elasticfilesystem:ClientWrite"
+#             ],
+#             "Condition": {
+#                 "Bool": {
+#                     "aws:SecureTransport": "true"
+#                 }
+#             }
+#         }
+#     ]
+# }
+# POLICY
+# }
--- image-build.tf
+++ image-build.tf
@@ -20,10 +20,18 @@ resource "aws_imagebuilder_component" "example" {
 }
 
 
-# resource "aws_imagebuilder_distribution_configuration" "example" {
-#   name = "example"
-#   kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
-#   distribution {
-#     region = "us-east-1"
-#   }
-# }
+resource "aws_imagebuilder_distribution_configuration" "example" {
+  name = "example"
+  kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
+  distribution {
+    ami_distribution_configuration {
+      ami_tags = {
+        CostCenter = "IT"
+      }
+
+      name = "example-{{ imagebuilder:buildDate }}"
+    }
+
+    region = "us-east-1"
+  }
+}
