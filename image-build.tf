eb810d22ce17353297052c36adc8c514a8fa1c0f
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-09-02 19:08:07 +0530

    Updating README file

--- image-build.tf
+++ image-build.tf
@@ -25,7 +25,6 @@ resource "aws_imagebuilder_component" "example" {
 
 resource "aws_imagebuilder_distribution_configuration" "example" {
   name = "example"
-  kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
   distribution {
     ami_distribution_configuration {
       ami_tags = {
@@ -33,7 +32,7 @@ resource "aws_imagebuilder_distribution_configuration" "example" {
       }
 
       name = "example-{{ imagebuilder:buildDate }}"
-
+      kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
       launch_permission {
         user_ids = ["123456789012"]
       }
