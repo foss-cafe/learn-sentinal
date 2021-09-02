34798c5f9e16d37d28735a05cfdab69ce4969ea3
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2021-09-02 08:14:50 +0530

    Updating README file

--- image-build.tf
+++ image-build.tf
@@ -3,7 +3,6 @@ resource "aws_imagebuilder_component" "example" {
   platform = "Linux"
   version  = "1.0.0"
   kms_key_id = "arn:aws:kms:ap-south-1:549942532493:key/f03fe8d0-8951-41eb-9f39-0ab438cd9002"
-  tags = var.tags
 }
 
 
@@ -13,5 +12,4 @@ resource "aws_imagebuilder_distribution_configuration" "example" {
   distribution {
     region = "us-east-1"
   }
-  tags = var.tags
 }
